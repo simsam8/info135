b147588e36c2deff0b2a0e48c8381e421983302e
simsam8 <simvedaa@gmail.com>
2024-02-27 09:00:39 +0100

    format file

--- assignments/zik018_assignment03.py
+++ assignments/zik018_assignment03.py
@@ -1,6 +1,8 @@
 import hashlib as hl
 import random
+
 # Task 1
+
 
 def hash_function(key: int, m: int) -> int:
     return key % m
@@ -31,7 +33,7 @@ for key in keys_task_2:
     else:
         i = hashed_key
         while hash_table[i] is not None:
-            i+=1
+            i += 1
         hash_table[i] = key
 
 print("Hash table insertion with Linear Probing:")
@@ -40,12 +42,13 @@ print(hash_table)
 
 # Task 3
 
+
 class HashClass:
     def __init__(self, id: int) -> None:
         self.id = id
 
     def hash_it(self) -> str:
-        salted_id = str(self.id + random.randint(1,1000))
+        salted_id = str(self.id + random.randint(1, 1000))
         hash_id = hl.sha1(salted_id.encode()).hexdigest()
         return hash_id
 
@@ -60,15 +63,17 @@ my_hash.print_it()
 
 # Task 4
 
+
 def most_frequent_integer(numbers: list[int]) -> int:
     frequency_table = {}
     for n in numbers:
         if n in frequency_table.keys():
             frequency_table[n] += 1
         else:
-            frequency_table.update({n:1})
+            frequency_table.update({n: 1})
 
-    return max(frequency_table, key= lambda n: frequency_table[n])
+    return max(frequency_table, key=lambda n: frequency_table[n])
+
 
 numbers = [10, 2, 5, 2, 3, 5, 6, 8, 5, 10]
 result = most_frequent_integer(numbers)
